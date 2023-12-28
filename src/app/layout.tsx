50cd35ce33b2da4627da1b5bc4149a3d21de80df
anderson-vilela <andersonvilela.dev@gmail.com>
2023-12-28 12:52:18 -0300

    feat: defining default variables

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,23 +1,23 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Montserrat } from 'next/font/google'
 import { ReactNode } from 'react'
 import './globals.css'
 
-const inter = Inter({ subsets: ['latin'] })
+const monstserrat = Montserrat({
+  weight: ['400', '500', '600', '700'],
+  subsets: ['latin'],
+})
 
 export const metadata: Metadata = {
   title: 'Next-SUS',
-  description: 'Desenvolvimento para o projeto avaliativo do Edital 028/2023 - LAIS / HUOL / UFRN. O projeto foi desenvolvido em React.js / Next.js pelo candidato Anderson Vilela.',
+  description:
+    'Desenvolvimento para o projeto avaliativo do Edital 028/2023 - LAIS / HUOL / UFRN. O projeto foi desenvolvido em React.js / Next.js pelo candidato Anderson Vilela.',
 }
 
-export default function RootLayout({
-  children,
-}: {
-  children: ReactNode
-}) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="pt-BR">
-      <body className={inter.className}>{children}</body>
+      <body className={monstserrat.className}>{children}</body>
     </html>
   )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,7 +7,28 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary-color': {
+          light: '#F6303F',
+          DEFAULT: '#D2202C',
+          dark: '#9f1821',
+        },
+        'secundary-color': {
+          light: '#707070',
+          DEFAULT: '#323237',
+          dark: '#030304',
+        },
+        neutral: {
+          0: '#FFFFFF',
+          100: '#F5F5F7',
+          200: '#E0E0E0',
+          300: '#707070',
+          400: '#2F2E41 ',
+          500: '#000000',
+        },
+      },
+    },
   },
   plugins: [],
 }
