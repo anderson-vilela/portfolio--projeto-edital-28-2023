050910ad1f8eba740ec2c60f9ac7fdf8982455c6
anderson-vilela <andersonvilela.dev@gmail.com>
2024-01-02 16:26:13 -0300

    feat: completion of the "/courses/:id" page

--- src/app/courses/[id]/page.tsx
+++ src/app/courses/[id]/page.tsx
@@ -1,5 +1,23 @@
 import Footer from '@/components/Footer'
 import Header from '@/components/Header'
+import { APIDataType } from '@/types/TypesAPI'
+import Image from 'next/image'
+import Link from 'next/link'
+
+const GetCourseDetails = async (id: number): Promise<APIDataType> => {
+  const response = await fetch(
+    `${process.env.URL_API_JSON_SERVER}/cursos/${id}`,
+    { next: { revalidate: 3600, tags: ['courseDetails'] } },
+  )
+
+  if (!response.ok) {
+    throw new Error('Failed to fetch data')
+  }
+
+  const data = await response.json()
+
+  return data
+}
 
 type CourseDetailsProps = {
   params: {
@@ -7,11 +25,138 @@ type CourseDetailsProps = {
   }
 }
 
-const CourseDetails = ({ params }: CourseDetailsProps) => {
+const CourseDetails = async ({ params }: CourseDetailsProps) => {
+  const course = await GetCourseDetails(params.id)
+
   return (
     <>
       <Header />
-      <main className="flex-1">{params.id}</main>
+      <main className="flex-1">
+        <div className="relative mt-[100px] min-h-[270px] p-8">
+          <nav className="container">
+            <ul className="flex items-center gap-2 font-semibold text-tw-neutral-200">
+              <li>
+                <Link
+                  className="drop-shadow-[2px_2px_1px_rgba(0,0,0,0.5)]"
+                  href={'/home'}
+                >
+                  Início
+                </Link>
+              </li>
+              <li>
+                <Link
+                  className="drop-shadow-[2px_2px_1px_rgba(0,0,0,0.5)]"
+                  href={'/courses'}
+                >
+                  / Cursos
+                </Link>
+              </li>
+              <li>
+                <Link
+                  className="drop-shadow-[2px_2px_1px_rgba(0,0,0,0.5)]"
+                  href={'/courses'}
+                >
+                  / Módulos
+                </Link>
+              </li>
+              <li className="text-tw-neutral-0 drop-shadow-[2px_2px_1px_rgba(0,0,0,0.5)]">
+                <Link href={`/courses/${params.id}`}>/ {course.titulo}</Link>
+              </li>
+            </ul>
+          </nav>
+          <Image
+            src={course.capa}
+            alt={course.titulo}
+            quality={100}
+            className="-z-10 h-full w-full bg-cover grayscale-[40%]"
+            sizes="100vw"
+            fill
+          />
+          <div className="container">
+            <h1 className="mt-[60px] text-[40px] font-bold leading-none text-tw-neutral-0 drop-shadow-[2px_2px_1px_rgba(0,0,0,0.5)]">
+              {course.titulo}
+            </h1>
+            <span className="mt-8 block font-serif text-[25px] text-tw-neutral-0 drop-shadow-[2px_2px_1px_rgba(0,0,0,0.5)]">
+              {course.parceiros}
+            </span>
+          </div>
+        </div>
+        <section className="container mt-8">
+          <h2 className="text-center text-[40px] font-semibold text-tw-primary-color">
+            Informações Gerais do Curso
+          </h2>
+          <div className="mt-[45px] flex items-center justify-between">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+          <div className="mt-[45px]">
+            <h3 className="text-center text-[25px] font-semibold text-tw-primary-color">
+              Sobre o curso
+            </h3>
+            <p className="mt-8 text-justify text-base font-normal text-black">
+              {course.sobre}
+            </p>
+          </div>
+          {course.objetivo_geral || course.objetivo_especifico ? (
+            <div className="mt-[30px]">
+              <h3 className="text-center text-[25px] font-semibold text-tw-primary-color">
+                Objetivos
+              </h3>
+              {course.objetivo_geral && (
+                <div className="mt-[30px]">
+                  <h4 className="text-base font-bold text-black">
+                    Objetivo Geral
+                  </h4>
+                  <p className="mt-3">{course.objetivo_geral}</p>
+                </div>
+              )}
+              {course.objetivo_especifico && (
+                <div className="mt-[30px]">
+                  <h4 className="text-base font-bold text-black">
+                    Objetivos Específicos
+                  </h4>
+                  <p className="mt-3 text-black">
+                    {course.objetivo_especifico}
+                  </p>
+                </div>
+              )}
+            </div>
+          ) : null}
+          {course.recursos_educacionais && (
+            <div className="mt-[30px]">
+              <h3 className="text-center text-[25px] font-semibold text-tw-primary-color">
+                Recursos educacionais
+              </h3>
+              <p className="mt-8 text-center text-base font-normal text-black">
+                {course.recursos_educacionais}
+              </p>
+            </div>
+          )}
+          <div className="mb-[140px] mt-16">
+            <h3 className="text-center text-[25px] font-semibold text-tw-primary-color">
+              Créditos
+            </h3>
+            <div className="mt-8 grid grid-cols-4 content-center gap-10">
+              {course.creditos.map((credit, index) => (
+                <div
+                  key={index}
+                  className="flex h-full w-full items-center justify-center"
+                >
+                  <Image
+                    src={credit.capa}
+                    alt={credit.titulo}
+                    className="bg-cover"
+                    height={400}
+                    width={300}
+                  />
+                </div>
+              ))}
+            </div>
+          </div>
+        </section>
+      </main>
       <Footer />
     </>
   )
