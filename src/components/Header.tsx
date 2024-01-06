d518b52000b16e98205ebb7bbb45c63eacef4998
anderson-vilela <andersonvilela.dev@gmail.com>
2024-01-06 17:10:44 -0300

    feat: building "layout responsive" for header - v0.01

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,58 +3,82 @@ import NavLink from '@/fragments/NavLink'
 import Image from 'next/image'
 import Link from 'next/link'
 import { HiMagnifyingGlass } from 'react-icons/hi2'
+import { RiMenuLine } from 'react-icons/ri'
 
 const Header = () => {
   return (
-    <header className="fixed left-0 top-0 z-50 flex w-full items-center justify-between gap-8 bg-tw-neutral-0 px-20 py-5 text-xl font-bold text-tw-neutral-400 drop-shadow-md">
-      <div>
-        <Link href="/home">
-          <Image src={logoAvaSus} alt="Logomarca" width={200} />
-        </Link>
-      </div>
-      <div className="flex items-center gap-4">
-        <nav className="px-8 py-4">
-          <ul className="flex items-center justify-center gap-9">
-            <li>
-              <NavLink href="/home">Início</NavLink>
-            </li>
-            <li>
-              <NavLink href="/about">Sobre Nós</NavLink>
-            </li>
-            <li>
-              <NavLink href="/courses">Cursos</NavLink>
-            </li>
-            <li>
-              <NavLink href="/partners">Parceiros</NavLink>
-            </li>
-            <li>
-              <NavLink href="/transparency">Transparência</NavLink>
-            </li>
-            <li>
-              <NavLink href="/contacts">Contato</NavLink>
-            </li>
-          </ul>
-        </nav>
-        <label
-          htmlFor="header-search"
-          className="group flex w-[21.875rem] items-center rounded-[20px] border-2 border-tw-secundary-color-dark/20 bg-transparent px-4 text-tw-secundary-color-light duration-300 hover:cursor-text hover:border-tw-primary-color-dark"
-        >
-          <HiMagnifyingGlass className="text-tw-secundary-color duration-300 group-hover:text-tw-primary-color-dark" />
-          <input
-            type="search"
-            name="search"
-            id="header-search"
-            placeholder="Buscar por um assunto..."
-            className="w-full bg-transparent p-2 text-base text-tw-secundary-color-light outline-none duration-300 placeholder:font-normal placeholder:text-tw-secundary-color group-hover:placeholder:text-tw-primary-color-dark"
-          />
-        </label>
+    <header className="fixed left-0 top-0 z-50 grid min-h-[100px] w-full gap-2 bg-tw-neutral-0 px-5 py-5 font-bold text-tw-neutral-400 drop-shadow-md xl:px-10 2xl:text-[18px] min-[1800px]:gap-4 min-[1800px]:px-20 min-[1800px]:text-xl">
+      <div className="flex w-full items-center justify-between gap-8">
+        <div className="flex items-center gap-2">
+          <Link href="/home">
+            <Image
+              src={logoAvaSus}
+              alt="Logomarca"
+              width={200}
+              className="h-[22px] w-[100px] xl:h-[33px] xl:w-[150px] 2xl:h-[44px] 2xl:w-[200px]"
+            />
+          </Link>
+          <div className="h-[44px] w-[2px] rounded-full bg-tw-neutral-300 min-[1100px]:hidden" />
+          <div className="flex items-center gap-2 min-[1100px]:hidden">
+            <RiMenuLine className="text-xl" />
+            <span>Menu</span>
+          </div>
+        </div>
         <div className="flex items-center gap-4">
-          <button className="rounded-[20px] border border-tw-secundary-color-light bg-transparent px-7 py-2 font-semibold text-tw-secundary-color-light duration-300 hover:border-tw-primary-color-dark hover:bg-tw-primary-color-dark hover:text-tw-neutral-0">
-            Entrar
-          </button>
-          <button className="rounded-[20px] bg-tw-secundary-color-light px-7 py-2 font-semibold text-tw-neutral-0 hover:border-tw-primary-color-dark hover:bg-tw-primary-color-dark">
-            Cadastrar
-          </button>
+          <nav className="hidden pr-2 min-[1100px]:block xl:pr-4 min-[1850px]:py-2 min-[1920px]:px-8 min-[1920px]:py-4">
+            <ul className="flex flex-wrap items-center justify-center gap-x-4 gap-y-2 min-[1800px]:gap-x-9">
+              <li>
+                <NavLink href="/home">Início</NavLink>
+              </li>
+              <li>
+                <NavLink href="/about">Sobre Nós</NavLink>
+              </li>
+              <li>
+                <NavLink href="/courses">Cursos</NavLink>
+              </li>
+              <li>
+                <NavLink href="/partners">Parceiros</NavLink>
+              </li>
+              <li>
+                <NavLink href="/transparency">Transparência</NavLink>
+              </li>
+              <li>
+                <NavLink href="/contacts">Contato</NavLink>
+              </li>
+            </ul>
+          </nav>
+          <form action="" method="get">
+            <label
+              htmlFor="header-search"
+              className="group flex w-[130px] items-center rounded-[16px] border-2 border-tw-secundary-color-dark/20 bg-transparent text-tw-secundary-color-light duration-300 hover:cursor-text hover:border-tw-primary-color-dark 2xl:w-[240px] 2xl:px-2 min-[1800px]:rounded-[20px] min-[1920px]:w-[21.875rem] min-[1920px]:px-4"
+            >
+              <div className="hidden min-[1920px]:block">
+                <HiMagnifyingGlass className="text-tw-secundary-color duration-300 group-hover:text-tw-primary-color-dark" />
+              </div>
+              <input
+                type="search"
+                name="search"
+                id="header-search"
+                placeholder="Pesquisar..."
+                className="w-full bg-transparent px-2 py-1 text-base text-tw-secundary-color-light outline-none duration-300 placeholder:font-normal placeholder:text-tw-secundary-color group-hover:placeholder:text-tw-primary-color-dark 2xl:hidden min-[1800px]:p-2"
+              />
+              <input
+                type="search"
+                name="search"
+                id="header-search"
+                placeholder="Buscar por um assunto..."
+                className="hidden w-full bg-transparent px-2 py-1 text-base text-tw-secundary-color-light outline-none duration-300 placeholder:font-normal placeholder:text-tw-secundary-color group-hover:placeholder:text-tw-primary-color-dark 2xl:block min-[1800px]:p-2"
+              />
+            </label>
+          </form>
+          <div className="flex items-center gap-4">
+            <button className="rounded-[16px] border border-tw-secundary-color-light bg-transparent px-4 py-1 font-semibold text-tw-secundary-color-light duration-300 hover:border-tw-primary-color-dark hover:bg-tw-primary-color-dark hover:text-tw-neutral-0 min-[1800px]:rounded-[20px] min-[1800px]:px-7 min-[1800px]:py-2">
+              Entrar
+            </button>
+            <button className="rounded-[16px] bg-tw-secundary-color-light px-4 py-1 font-semibold text-tw-neutral-0 hover:border-tw-primary-color-dark hover:bg-tw-primary-color-dark min-[1800px]:rounded-[20px] min-[1800px]:px-7 min-[1800px]:py-2">
+              Cadastrar
+            </button>
+          </div>
         </div>
       </div>
     </header>
---
+++ src/components/SearchButton.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+import { FaLevelDownAlt } from 'react-icons/fa'
+
+const SearchButton = () => {
+  return (
+    <button className="flex items-center">
+      Pesquisar
+      <FaLevelDownAlt />
+    </button>
+  )
+}
+
+export default SearchButton
